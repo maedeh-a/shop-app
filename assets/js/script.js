60cd3a4ec55eb299f0b08e65512b06ca0abe144c
abbasebadian <abbasebadiann@gmail.com>
2023-12-29 16:56:34 +0330

    toggle-button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 $(function () {
 
+  
+
+    $("[toggle-button-group] [toggle-button]").on("click", function (e) {
+        console.log("re");
+        const $current = $(e.currentTarget)
+        const $siblings = $current.siblings('[toggle-button]')
+
+        $siblings.removeClass('active')
+        $current.addClass('active')
+    })
     $("#rateYo").rateYo({
         rating: 3.6
     });
-
 });
